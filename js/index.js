5ae8e88ec629ce3274005bad7272f3ca491b21f2
ievgenbugai <tyz.kiev.ua@gmail.com>
2020-09-21 18:07:49 +0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
-let message = 'Hello World';//дали имя переменной message
-alert('Hello World');
+// let message = 'Hello World';//дали имя переменной message//String
+// alert('Hello World');
+
+//Числа
+// const sum = 2 + 2;
+// alert(sum);
+
+//prompt - возврат //parseFloat - не целое число. Роевращаем текст в цифру
+const age = parseFloat(prompt('Enter age'));
+alert('Your age is' + age);
+
+const nextAge = add(age, 1);
+alert('Next age is' + nextAge);
+
+if(age < 18) {  
+    alert('You under age'); 
+
+} else { 
+    alert('You are adult!');
+}
+
+function add(a, b) { 
+    const sum = a + b;
+    return sum;
+}
